1198dbf80b240429632922081c5024421ad93a4e
Andrey Pichugin <a-pichugin@yandex-team.ru>
2015-08-05 16:46:28 +0300

    sphere works

--- assignment3/Sphere.js
+++ assignment3/Sphere.js
@@ -3,13 +3,6 @@
 var canvas;
 var gl;
 
-var numTimesToSubdivide = 3;
-
-var index = 0;
-
-var pointsArray = [];
-
-
 var near = -10;
 var far = 10;
 var radius = 6.0;
@@ -17,48 +10,15 @@ var theta  = 0.0;
 var phi    = 0.0;
 var dr = 5.0 * Math.PI/180.0;
 
+var numTimesToSubdivide = 5;
+
 var left = -2.0;
 var right = 2.0;
 var ytop = 2.0;
 var bottom = -2.0;
 
-var modelViewMatrix, projectionMatrix;
-var modelViewMatrixLoc, projectionMatrixLoc;
-var eye;
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
-
-function triangle(a, b, c) {
-     pointsArray.push(a);
-     pointsArray.push(b);
-     pointsArray.push(c);
-     index += 3;
-}
-
-
-function divideTriangle(a, b, c, count) {
-    if ( count > 0 ) {
-
-        var ab = normalize(mix( a, b, 0.5), true);
-        var ac = normalize(mix( a, c, 0.5), true);
-        var bc = normalize(mix( b, c, 0.5), true);
-
-        divideTriangle( a, ab, ac, count - 1 );
-        divideTriangle( ab, b, bc, count - 1 );
-        divideTriangle( bc, c, ac, count - 1 );
-        divideTriangle( ab, bc, ac, count - 1 );
-    }
-    else { // draw tetrahedron at end of recursion
-        triangle( a, b, c );
-    }
-}
-
-function tetrahedron(a, b, c, d, n) {
-    divideTriangle(a, b, c, n);
-    divideTriangle(d, c, b, n);
-    divideTriangle(a, d, b, n);
-    divideTriangle(a, c, d, n);
-}
 
 window.onload = function init() {
     canvas = document.getElementById( "gl-canvas" );
@@ -67,75 +27,18 @@ window.onload = function init() {
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.enable(gl.DEPTH_TEST);
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
     //
-    //  Load shaders and initialize attribute buffers
+    //  Load shaders
     //
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    var va = vec4(0.0, 0.0, -1.0, 1);
-    var vb = vec4(0.0, 0.942809, 0.333333, 1);
-    var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
-    var vd = vec4(0.816497, -0.471405, 0.333333, 1);
-
-    tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
-
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
-
-    var vPosition = gl.getAttribLocation( program, "vPosition");
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray( vPosition);
-
-    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
-
-    document.getElementById("Button0").onclick = function(){theta += dr;};
-    document.getElementById("Button1").onclick = function(){theta -= dr;};
-    document.getElementById("Button2").onclick = function(){phi += dr;};
-    document.getElementById("Button3").onclick = function(){phi -= dr;};
-
-    document.getElementById("Button4").onclick = function(){
-        numTimesToSubdivide++;
-        index = 0;
-        pointsArray = [];
-        init();
-    };
-    document.getElementById("Button5").onclick = function(){
-        if(numTimesToSubdivide) numTimesToSubdivide--;
-        index = 0;
-        pointsArray = [];
-        init();
-    };
-    render();
+    var sphere1 = new Sphere(radius, numTimesToSubdivide);
+    sphere1.render(gl, program);
 }
-
-
-function render() {
-
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    eye = vec3(radius*Math.sin(theta)*Math.cos(phi),
-        radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
-
-    modelViewMatrix = lookAt(eye, at , up);
-    projectionMatrix = ortho(left, right, bottom, ytop, near, far);
-
-    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
-
-
-    for( var i=0; i<index; i+=3)
-       gl.drawArrays( gl.LINE_LOOP, i, 3 );
-
-    window.requestAnimFrame(render);
-
-
-}
-
 
 function Sphere(radius, numTimesToSundivide)
 {
@@ -151,67 +54,78 @@ function Sphere(radius, numTimesToSundivide)
     var vc = vec4(-0.816497, -0.471405, 0.333333, 1);
     var vd = vec4(0.816497, -0.471405, 0.333333, 1);
 
-    function triangle(a, b, c) {
-        pointsArray.push(a);
-        pointsArray.push(b);
-        pointsArray.push(c);
-        index += 3;
+    this.triangle = function triangle(a, b, c) {
+        this.pointsArray.push(a);
+        this.pointsArray.push(b);
+        this.pointsArray.push(c);
+        this.index += 3;
     }
 
-    function divideTriangle(a, b, c, count) {
+    this.divideTriangle = function divideTriangle(a, b, c, count) {
         if ( count > 0 ) {
 
             var ab = normalize(mix( a, b, 0.5), true);
             var ac = normalize(mix( a, c, 0.5), true);
             var bc = normalize(mix( b, c, 0.5), true);
 
-            divideTriangle( a, ab, ac, count - 1 );
-            divideTriangle( ab, b, bc, count - 1 );
-            divideTriangle( bc, c, ac, count - 1 );
-            divideTriangle( ab, bc, ac, count - 1 );
+            this.divideTriangle( a, ab, ac, count - 1 );
+            this.divideTriangle( ab, b, bc, count - 1 );
+            this.divideTriangle( bc, c, ac, count - 1 );
+            this.divideTriangle( ab, bc, ac, count - 1 );
         }
         else { // draw tetrahedron at end of recursion
-            triangle( a, b, c );
+            this.triangle( a, b, c );
         }
     }
 
-    function tetrahedron(a, b, c, d, n) {
-        divideTriangle(a, b, c, n);
-        divideTriangle(d, c, b, n);
-        divideTriangle(a, d, b, n);
-        divideTriangle(a, c, d, n);
+    this.tetrahedron = function tetrahedron(a, b, c, d, n) {
+        this.divideTriangle(a, b, c, n);
+        this.divideTriangle(d, c, b, n);
+        this.divideTriangle(a, d, b, n);
+        this.divideTriangle(a, c, d, n);
     }
 
     //construction
-    tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
+    this.tetrahedron(va, vb, vc, vd, numTimesToSubdivide);
 
     this.vBuffer = null;
     this.eye = vec3(radius*Math.sin(theta)*Math.cos(phi),
                     radius*Math.sin(theta)*Math.sin(phi),
                     radius*Math.cos(theta));
 
-    this.modelViewMatrix = lookAt(eye, at, up);
-    this.modelViewMatrixLoc = null;
+    this.modelViewMatrix = lookAt(this.eye, at, up);
 
-    this.projectionMatrix =  = ortho(left, right, bottom, ytop, near, far);
-    this.projectionMatrixLoc = null;
+    this.projectionMatrix = ortho(left, right, bottom, ytop, near, far);
 
-    this.vPosition = null;
+    this.vBuffer = gl.createBuffer();
+
 
     this.render = function render(gl, program)
     {
-        if (!vBuffer)
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.vBuffer);
+        gl.bufferData( gl.ARRAY_BUFFER, flatten(this.pointsArray), gl.STATIC_DRAW);
+
+        var vPosition = gl.getAttribLocation( program, "vPosition");
+        gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(vPosition);
+
+        var modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+        var projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+        var colorLoc = gl.getUniformLocation( program, "color" );
+
+        gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(this.modelViewMatrix) );
+        gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(this.projectionMatrix) );
+
+        gl.uniform4f( colorLoc, 1.0, 0.0, 0.0, 1.0 );
+        gl.drawArrays( gl.TRIANGLES, 0, this.index );
+
+        for( var i=0; i< this.index; i+=3)
         {
-            vBuffer = gl.createBuffer();
-            gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-            gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
-
-            var vPosition = gl.getAttribLocation( program, "vPosition");
-            gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0);
-            gl.enableVertexAttribArray( vPosition);
-
-            modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-            projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+            gl.uniform4f( colorLoc, 0.0, 0.0, 0.0, 1.0 );
+            gl.drawArrays( gl.LINE_LOOP, i, 3 );
         }
+
+        gl.bindBuffer( gl.ARRAY_BUFFER, null);
+        gl.disableVertexAttribArray(vPosition);
     }
 }
